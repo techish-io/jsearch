3126903dfe3759dccf53416b31e557ddebabe662
Ishtiaq Ahmed <Ishtiaq@Ishtiaqs-MacBook-Pro.local>
2019-12-15 11:22:25 +1100

    Added unit tests

---
+++ tests/test_add_data.py
@@ -0,0 +1,22 @@
+import functions as myfunc
+import pandas as pd
+import unittest
+
+class TestAdd_Data(unittest.TestCase):
+    """
+    Test the add_data function from the functions library
+    """
+ 
+    def test_add_data(self):
+        """
+        Test that function successfully adds column to output dataframe
+        """
+        output_pd = pd.DataFrame([['tom', 10]], columns = ['Name', 'Age'])
+        input_pd = pd.DataFrame([[21.0, True]], columns = ['Points', 'Active'])
+
+        result = myfunc.add_data(input_pd, output_pd,  "Active", "Active")
+        self.assertEqual( len(result.columns), len(input_pd.columns) + 1 )
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_type_cast.py
@@ -0,0 +1,40 @@
+import functions as myfunc
+import pandas as pd
+import unittest
+
+class TestType_Cast(unittest.TestCase):
+    """
+    Test the type_cast function from the functions library
+    """
+ 
+    def test_type_cast_str(self):
+        """
+        Test the type cast of dataframe colum of type string
+        """
+        result = myfunc.type_cast(pd.DataFrame([['tom', 10, 21.0, True]], columns = ['Name', 'Age', 'Points', 'Active'])['Name'], "tom")
+        self.assertEqual(result, str("tom"))
+
+    def test_type_cast_int(self):
+        """
+        Test the type cast of dataframe colum of type int
+        """
+        result = myfunc.type_cast(pd.DataFrame([['tom', 10, 21.0, True]], columns = ['Name', 'Age', 'Points', 'Active'])['Age'], 10)
+        self.assertEqual(result, 10)
+
+    def test_type_cast_float(self):
+        """
+        Test the type cast of dataframe colum of type float
+        """
+        result = myfunc.type_cast(pd.DataFrame([['tom', 10, 21.0, True]], columns = ['Name', 'Age', 'Points', 'Active'])['Points'],21.0)
+        self.assertEqual(result, 21.0)
+    
+    def test_type_cast_bool(self):
+        """
+        Test the type cast of dataframe colum of type bool
+        """
+        result = myfunc.type_cast(pd.DataFrame([['tom', 10, 21.0, True]], columns = ['Name', 'Age', 'Points', 'Active'])['Active'], True)
+        self.assertEqual(result, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
