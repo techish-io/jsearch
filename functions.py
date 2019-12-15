33fc5b6e36b9563f495e5b83bc37469bde75c448
Ishtiaq Ahmed <Ishtiaq@Ishtiaqs-MacBook-Pro.local>
2019-12-15 12:33:57 +1100

    Cleanup

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 def display_output(output_rows_df):
     for index, output_row in output_rows_df.head().iterrows():
+        print("---------------------------------------------------------------------")
         print(output_row.to_string(header=None))
         print("")
 
